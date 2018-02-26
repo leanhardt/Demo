a49215a9c78ef3159d20ad3d555f09416ab80a38
willbin <893607696.com>
2018-02-26 16:03:40 +0800

    修改显示内容

--- Flask_demo/apps/front/models.py
+++ Flask_demo/apps/front/models.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from exts import db
-from sqlalchemy import Column,Integer
+import shortuuid
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
-class CMSUser(db.Model):
-    __tablename__ = 'cms_user'
-    id = db.column('id',Integer,prefix_key=True)
+class FrontUser(db.Model):
+    __tablename__ = 'front_user'
+    id = db.Column(db.String(50),primary_key=True,default=shortuuid.uuid)
+    telephone = db.Column(db.String(11),nullable=False,unique=True)
+    _password = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(50))
+    avatar = db.Column(db.String(100))
+    join_time = db.Column(db.DateTime,default=datetime.now)
+
+    def __init__(self,*args,**kwargs):
+        if 'password' in kwargs:
+            self._password = kwargs.get('password')
+        
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self,raw_password):
+        self._password = generate_password_hash(raw_password)
+
+    def check_password(self,raw_password):
+        return check_password_hash(self.password,raw_password)
+
+
+
